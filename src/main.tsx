926c96c8f6aa951c3327fd624a903b30170a9a98
bonkebon <fueralmar@gmail.com>
2024-08-14 16:58:20 -0300

    On branch pag 	renamed:    public/fndo.jpeg -> public/icon.png 	modified:   src/main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import App from './App';
 import './index.css';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
-const manifestUrl = 'https://github.com/MarceloFuertes/FAMCBA.github.io/blob/pag/dist/tonconnect-manifest.json';
+const manifestUrl = 'https://github.com/MarceloFuertes/FAMCBA.github.io/tonconnect-manifest.json';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
